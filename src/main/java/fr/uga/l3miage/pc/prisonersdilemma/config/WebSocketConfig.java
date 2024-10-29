d55bcd93b0b3125fc34141b170f1a09abc8c1906
typhon150 <bsupmac@gmail.com>
2024-10-29 22:14:36 +0100

    jai remis .withSockJS(), a l'air de faire planter postman mais usremtn mieux une fois avec angualr

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/config/WebSocketConfig.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/server").setAllowedOrigins("*");
+        registry.addEndpoint("/server").setAllowedOrigins("*").withSockJS();
     }
 }
