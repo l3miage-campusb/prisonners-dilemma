e0e6a13cda7a097c596c4955d09dfc5cb696fb52
dominger <ernesto.dominguez-montesino@etu.univ-grenoble-alpes.fr>
2024-11-09 17:09:43 +0100

    Communication reussite finel

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controller/GameController.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controller/GameController.java
@@ -2,6 +2,7 @@ package fr.uga.l3miage.pc.prisonersdilemma.controller;
 
 import fr.uga.l3miage.pc.prisonersdilemma.model.ChoiceMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -21,10 +22,21 @@ public class GameController {
         String result = processChoices(message);
 
         // Envoyer le résultat à tous les clients abonnés au topic /topic/results
-        messagingTemplate.convertAndSend("/topic/results", result);
+        messagingTemplate.convertAndSend("/topic/results", "peneeeeeeeee");
     }
 
-    // Exemple de traitement des choix
+    // Méthode pour gérer les rounds
+    @MessageMapping("/round")  // Le message envoyé à /app/round sera traité ici
+    @SendTo("/topic/round")
+    public String  handleRounds(String nbRound) {
+
+        // Traitement du nombre de rounds, par exemple un simple calcul ou gestion
+
+        // Envoie du résultat à tous les clients abonnés au topic /topic/results
+        return nbRound;
+    }
+
+    // Exemple de trait ement des choix
     private String processChoices(ChoiceMessage message) {
         // Logique pour traiter les choix et retourner le résultat
         return "Résultat pour le choix de " + message.getPlayerId() + ": " + message.getChoice();
