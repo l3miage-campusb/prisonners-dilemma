e106dbee4dbb493febdcafe09dedce5c6065a1c6
dominger <ernesto.dominguez-montesino@etu.univ-grenoble-alpes.fr>
2024-11-14 22:32:12 +0100

    Test sonar 2

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/model/strategies/Strategy.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/model/strategies/Strategy.java
@@ -8,14 +8,14 @@ public enum Strategy {
     DONNANT_POUR_DEUX_DONNANTS,         //fait,test
     SONDEUR_NAIF,               //fait,teste
     SONDEUR_REPENTANT,          //fait,teste
-    PACIFICATEUR_NAIF,              //fait
+    PACIFICATEUR_NAIF,              //fait,test
     VRAI_PACIFICATEUR,              //fait,teste
     ALEATOIRE,                  //fait,teste
     TOUJOURS_TRAHIR,                //fait,teste
     TOUJOURS_COOPERER,          //fait,teste
     RANCUNIER,                  //fait et teste
     PAVLOV,                     //fait et teste
-    PAVLOV_ALEATOIRE,           //fait
+    PAVLOV_ALEATOIRE,           //fait,
     ADAPTATIF,
     GRADUEL,
     DONNANT_DONNANT_SUPCONNEUX,
